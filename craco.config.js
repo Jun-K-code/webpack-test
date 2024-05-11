90b2a4d056241298800353ec4148d9da9ba7b41d
Jun-K-code <JunCode@aliyun.com>
2024-05-11 16:03:27 +0800

    fix: craco.config.js-注释css-loader的配置

--- craco.config.js
+++ craco.config.js
@@ -16,20 +16,19 @@ const configureWebpack = (webpackConfig, { env, paths }) => {
       ? 'assets/js/[name].[contenthash:8].chunk.js'
       : isEnvDevelopment && 'assets/js/[name].chunk.js',
   };
-  console.log('测试module', webpackConfig.module);
-  webpackConfig.module = {
-    ...webpackConfig.module,
-    rules: [
-      ...webpackConfig.module.rules,
-      {
-        test: /\.css$/i,
-        loader: 'css-loader',
-        options: {
-          modules: true,
-        },
-      },
-    ],
-  };
+  // webpackConfig.module = {
+  //   ...webpackConfig.module,
+  //   rules: [
+  //     ...webpackConfig.module.rules,
+  //     {
+  //       test: /\.css$/i,
+  //       loader: 'css-loader',
+  //       options: {
+  //         modules: true,
+  //       },
+  //     },
+  //   ],
+  // };
   return webpackConfig;
 };
 

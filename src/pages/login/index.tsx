807c34954891b4fcf6e2c0d18eef2c80c19cbaed
Jun-K-code <JunCode@aliyun.com>
2024-05-11 16:37:01 +0800

    fix: 测试xxx.module.css，测试结果是样式能正确渲染到页面上

--- src/pages/login/index.tsx
+++ src/pages/login/index.tsx
@@ -1,6 +1,8 @@
 import React from 'react';
 
+import styles from './index.module.css';
+
 const Login = () => {
-  return <div>测试</div>;
+  return <div className={styles.test}>测试</div>;
 };
 export default Login;

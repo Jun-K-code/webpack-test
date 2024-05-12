a2cd2c1389257630ba5f0f7041a65cffb56fdeb6
Jun-K-code <JunCode@aliyun.com>
2024-05-12 22:09:53 +0800

    fix: 测试图片资源

--- src/pages/login/index.tsx
+++ src/pages/login/index.tsx
@@ -3,6 +3,10 @@ import React from 'react';
 import styles from './index.module.css';
 
 const Login = () => {
-  return <div className={styles.test}>测试</div>;
+    return (
+        <div className={styles.test}>
+            测试
+        </div>
+    );
 };
 export default Login;

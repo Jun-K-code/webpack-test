0ee3f32dd4a374829f239a1e53424eef0cc466a7
Jun-K-code <JunCode@aliyun.com>
2024-05-12 22:17:48 +0800

    fix: craco.config.js-配置webpack-bundle-analyzer

--- craco.config.js
+++ craco.config.js
@@ -1,38 +1,44 @@
 /* global module __dirname process */
 const path = require('path');
+const WebpackBundleAnalyzer = require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
 
 const configureWebpack = (webpackConfig, { env, paths }) => {
-  const isEnvDevelopment = env === 'development';
-  const isEnvProduction = env === 'production';
-  webpackConfig.output = {
-    ...webpackConfig.output,
-    // There will be one main bundle, and one file per asynchronous chunk.
-    // In development, it does not produce real files.
-    filename: isEnvProduction
-      ? 'assets/js/[name].[contenthash:8].js'
-      : isEnvDevelopment && 'assets/js/bundle.js',
-    // There are also additional JS chunk files if you use code splitting.
-    chunkFilename: isEnvProduction
-      ? 'assets/js/[name].[contenthash:8].chunk.js'
-      : isEnvDevelopment && 'assets/js/[name].chunk.js',
-  };
-  return webpackConfig;
+    const isEnvDevelopment = env === 'development';
+    const isEnvProduction = env === 'production';
+    webpackConfig.output = {
+        ...webpackConfig.output,
+        // There will be one main bundle, and one file per asynchronous chunk.
+        // In development, it does not produce real files.
+        filename: isEnvProduction
+            ? 'assets/js/[name].[contenthash:8].js'
+            : isEnvDevelopment && 'assets/js/bundle.js',
+        // There are also additional JS chunk files if you use code splitting.
+        chunkFilename: isEnvProduction
+            ? 'assets/js/[name].[contenthash:8].chunk.js'
+            : isEnvDevelopment && 'assets/js/[name].chunk.js',
+    };
+    return webpackConfig;
 };
 
 module.exports = {
-  webpack: {
-    rules: [
-      {
-        test: /\.css$/i,
-        loader: 'css-loader',
-        options: {
-          modules: true,
+    webpack: {
+        rules: [
+            {
+                test: /\.css$/i,
+                loader: 'css-loader',
+                options: {
+                    modules: true,
+                },
+            },
+            // {
+            //     test: [/\.bmp$/, /\.gif$/, /\.jpe?g$/, /\.png$/, /\.svg$/],
+            //     type: 'asset/resource',
+            // },
+        ],
+        plugins: [new WebpackBundleAnalyzer()],
+        alias: {
+            src: path.resolve(__dirname, './src'),
         },
-      },
-    ],
-    alias: {
-      src: path.resolve(__dirname, './src'),
+        configure: configureWebpack,
     },
-    configure: configureWebpack,
-  },
 };

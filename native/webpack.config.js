4da01f81970e35d8c6e7da2a08d807666bea9592
Jun-K-code <JunCode@aliyun.com>
2024-06-17 12:10:58 +0800

    chore: native-webpack配置-添加style-loader、css-loader配置

--- native/webpack.config.js
+++ native/webpack.config.js
@@ -7,7 +7,23 @@ module.exports = {
   output: {
     filename: '[name].[contenthash].js', // 输出文件名
     path: path.resolve(__dirname, 'build'), // 输出的目录
-    clean: true, // 在生成文件之前清空 output 目录
+    clean: true, // 在生成文件之前，清空 output 目录
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: [
+          'style-loader',
+          {
+            loader: 'css-loader',
+            options: {
+              modules: true,
+            },
+          },
+        ],
+      },
+    ],
   },
   plugins: [
     new HtmlWebpackPlugin({

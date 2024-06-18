4774c92b8300dd927d2889aa2b2922173cbfd643
Jun-K-code <JunCode@aliyun.com>
2024-06-18 15:16:50 +0800

    feat: 添加xxx.module.scss测试

--- native/src/index.js
+++ native/src/index.js
@@ -1,5 +1,7 @@
 import { renderComponent } from './utils';
 import testCSSModuleComponent from './pages/testCSSModuleComponent';
+import testSassModuleComponent from './pages/testSassModuleComponent';
 
 const container = document.getElementById('root');
 renderComponent(container, testCSSModuleComponent);
+renderComponent(container, testSassModuleComponent);
---
+++ native/src/pages/testSassModuleComponent/index.js
@@ -0,0 +1,5 @@
+import styles from './index.module.scss';
+
+export default () => {
+  return `<div class="${styles.myClass}">test sass content</div>`;
+};

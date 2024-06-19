8f6bd585910d9568900bf99134df75cff88e4137
Jun-K-code <JunCode@aliyun.com>
2024-06-19 17:13:41 +0800

    chore: native-webpack-添加optimization优化配置

--- native/webpack.config.js
+++ native/webpack.config.js
@@ -1,4 +1,6 @@
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const TerserPlugin = require('terser-webpack-plugin');
+const CssMinimizerPlugin = require('css-minimizer-webpack-plugin');
 const path = require('path');
 
 const devConfig = require('./webpack.dev');
@@ -60,8 +62,45 @@ const baseConfig = {
   },
 };
 function getWebpackConfig(env) {
-  if (env === 'production') {
-    return { ...baseConfig, ...proConfig };
+  const isProduction = env === 'production';
+  const optimization = {
+    minimize: isProduction,
+    minimizer: [
+      new TerserPlugin({
+        terserOptions: {
+          format: {
+            comments: false, // 表示移除注释
+          },
+          parse: {
+            ecma: 8, // 表示解析 ECMAScript 2017 代码
+          },
+          compress: {
+            ecma: 5, // 表示压缩时，保持 ECMAScript 5 兼容性
+            warnings: false, // 表示不显示压缩警告
+            comparisons: false, // 表示禁用比较操作的压缩，因为存在问题（参见相关 GitHub 问题）
+            inline: 2, // 表示内联函数
+            drop_debugger: true, // 移除debugger语句
+          },
+          mangle: {
+            safari10: true, // 兼容 Safari 10
+          },
+          // keep_classnames: isEnvProductionProfile,
+          // keep_fnames: isEnvProductionProfile,
+          output: {
+            ecma: 5, // ES5 兼容性
+            comments: false, // 移除注释
+            ascii_only: true, // ASCⅡ编码
+          },
+        },
+      }),
+      new CssMinimizerPlugin(),
+    ],
+    splitChunks: {
+      chunks: 'all',
+    },
+  };
+  if (isProduction) {
+    return { ...baseConfig, optimization, ...proConfig };
   }
   return { ...baseConfig, ...devConfig };
 }

9644c806078c3e4cae27f335b09b8a6d20ba693a
Jun-K-code <JunCode@aliyun.com>
2024-08-11 19:03:22 +0800

    feat: mini-webpack项目-新增获取文件内容和依赖关系功能

--- mini-webpack/index.js
+++ mini-webpack/index.js
@@ -1,12 +1,37 @@
 import fs from 'fs';
+import parser from '@babel/parser';
+import traverse from '@babel/traverse';
 
 // 创建资源
 function createAsset() {
-  // 获取文件的内容
-  // 获取依赖关系
+  // 1、获取文件的内容
+  // AST -> 抽象语法树
 
-  const source = fs.readFileSync('./example/main.js');
-  console.log(source);
-  
-  return {};
+  const source = fs.readFileSync('./example/main.js', {
+    encoding: 'utf-8',
+  });
+  // console.log('测试source', source);
+
+  // 2、获取依赖关系
+  const ast = parser.parse(source, {
+    sourceType: 'module',
+  });
+  // console.log('测试ast', ast);
+
+  const deps = [];
+  // 遍历ast
+  traverse.default(ast, {
+    ImportDeclaration: ({ node }) => {
+      // console.log('测试import node', node.source.value);
+      deps.push(node.source.value);
+    },
+  });
+
+  return {
+    source,
+    deps,
+  };
 }
+
+const asset = createAsset();
+console.log('测试asset', asset);
